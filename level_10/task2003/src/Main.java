389621309af550a8c946159de61fc1b1918297b0
Ilhan Mustafa <ilhnmst@gmail.com>
2024-12-25 21:25:22 +0300

    task2003 solution

--- level_10/task2003/src/Main.java
+++ level_10/task2003/src/Main.java
@@ -1,9 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
+
 
 /*
 Знакомство с properties
