ad3985e532fbe8e8f4b59a9f0674a5bf2e93f0c4
Ilhan Mustafa <ilhnmst@gmail.com>
2025-01-06 08:38:17 +0300

    change Main

--- level_10/task2027/src/Main.java
+++ level_10/task2027/src/Main.java
@@ -1,13 +1,25 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Crossword crossword = new Crossword(new int[][]{
+        int[][] crossword = new int[][]{
                 {'f', 'd', 'e', 'r', 'l', 'k'},
                 {'u', 's', 'a', 'm', 'e', 'o'},
                 {'l', 'n', 'g', 'r', 'o', 'v'},
                 {'m', 'l', 'p', 'r', 'r', 'h'},
                 {'p', 'o', 'e', 'e', 'j', 'j'}
-        });
-        System.out.println(crossword.detectAllWords("home", "same", "sameo"));
+        };
+        System.out.println(detectAllWords(crossword, "home", "same"));
+        /*
+Ожидаемый результат
+home - (5, 3) - (2, 0)
+same - (1, 1) - (4, 1)
+         */
+    }
+
+    public static List<Word> detectAllWords(int[][] crossword, String... words) {
+        Crossword crossword1 = new Crossword(crossword);
+        return crossword1.detectAllWords(words);
     }
 
     public static class Word {
@@ -37,4 +49,3 @@ public class Main {
         }
     }
 }
-
